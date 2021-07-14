e8b3e5087370fac289208c771f5e970b2ac7d9a3
simonrobins <simonrobins@localhost>
2021-07-14 15:29:27 +0100

    Add countUniqueSKUs which returns the number of each SKU was passed in.

--- src/com/simonrobins/pe/PromotionEngine.java
+++ src/com/simonrobins/pe/PromotionEngine.java
@@ -1,7 +1,35 @@
 package com.simonrobins.pe;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PromotionEngine {
-	public static Double calculateOrderCost(final String orders) {
+	public static Double calculateOrderCost(final String skus) {
 		return 0.0;
+	}
+
+	private static Map<Character, Integer> countUniqueSKUs(final String skus) {
+		Map<Character, Integer> groupedSKUs = new HashMap<>();
+
+		for (int i = 0; i < skus.length(); i++) {
+			char sku = skus.charAt(i);
+
+			switch (sku) {
+			case 'A':
+			case 'B':
+			case 'C':
+				Integer count = groupedSKUs.get(sku);
+				if(null == count)
+					groupedSKUs.put(sku, 1);
+				else
+					groupedSKUs.replace(sku, count + 1);
+				break;
+			default:
+				System.out.println("Unrecognised SKU - ignoring.");
+				break;
+			}
+		}
+
+		return groupedSKUs;
 	}
 }
