9074f626825beb919330226c2faa26980bd1828e
simonrobins <simonrobins@localhost>
2021-07-14 17:35:44 +0100

    Extract the CD promotion calculation into it's own method to eliminate duplicate calculations.

--- src/com/simonrobins/pe/PromotionEngine.java
+++ src/com/simonrobins/pe/PromotionEngine.java
@@ -4,26 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PromotionEngine {
-	
-	private interface Function
-	{
+
+	private interface Function {
 		public Double calculate(Map<Character, Integer> skuMap);
 	}
-	
-	private static Function[] orderCostFunctions = { 
-			new FunctionA(),
-			new FunctionB(),
-			new FunctionCD()
-	};
-	
+
+	private static Function[] orderCostFunctions = { new FunctionA(), new FunctionB(), new FunctionCD() };
+
 	public static Double calculateOrderCost(final String skus) {
 
 		Map<Character, Integer> groupedSKUs = countUniqueSKUs(skus);
 
 		Double cost = 0.0;
 
-		for(Function function : orderCostFunctions)
-		{
+		for (Function function : orderCostFunctions) {
 			cost += function.calculate(groupedSKUs);
 		}
 
@@ -60,56 +54,49 @@ public class PromotionEngine {
 		return groupedSKUs;
 	}
 
-	private static class FunctionA implements Function
-	{
+	private static class FunctionA implements Function {
 		@Override
 		public Double calculate(Map<Character, Integer> skuMap) {
 			Integer numberOfAs = skuMap.get('A');
 
 			Integer numberOfPromotions = numberOfAs / 3;
 			Integer numberOfUnits = numberOfAs % 3;
-			
+
 			return numberOfPromotions * 130.0 + numberOfUnits * 50.0;
-		}		
+		}
 	}
 
-	private static class FunctionB implements Function
-	{
+	private static class FunctionB implements Function {
 		@Override
 		public Double calculate(Map<Character, Integer> skuMap) {
 			Integer numberOfBs = skuMap.get('B');
 
 			Integer numberOfPromotions = numberOfBs / 2;
 			Integer numberOfUnits = numberOfBs % 2;
-			
+
 			return numberOfPromotions * 45.0 + numberOfUnits * 30.0;
-		}		
+		}
 	}
 
-	private static class FunctionCD implements Function
-	{
+	private static class FunctionCD implements Function {
 		@Override
 		public Double calculate(Map<Character, Integer> skuMap) {
 			Integer numberOfCs = skuMap.get('C');
 			Integer numberOfDs = skuMap.get('D');
 
-			if(0 == numberOfCs)
-			{
-				return numberOfDs * 15.0;
+			if (0 == numberOfCs) {
+				return calculateTotal(0, 0, numberOfDs);
+			} else if (0 == numberOfDs) {
+				return calculateTotal(0, numberOfCs, 0);
+			} else if (numberOfCs > numberOfDs) {
+				return calculateTotal(numberOfCs, numberOfCs - numberOfDs, 0);
+			} else {
+				return calculateTotal(numberOfDs, 0, numberOfDs - numberOfCs);
 			}
-			else if(0 == numberOfDs)
-			{
-				return numberOfCs * 20.0;
-			}
-			else if(numberOfCs > numberOfDs)
-			{
-				return  ((numberOfCs - numberOfDs) * 20.0) + numberOfCs * 30.0;
-			}
-			else
-			{
-				return  ((numberOfDs - numberOfCs) * 15.0) + numberOfDs * 30.0;
-				
-			}
-		}		
+		}
+
+		private Double calculateTotal(Integer promotions, Integer unitsOfC, Integer unitsOfD) {
+			return promotions * 30.0 + unitsOfC * 20 + unitsOfD * 15;
+		}
 	}
 }
