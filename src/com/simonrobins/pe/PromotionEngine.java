4be4a76c1b13d153dde00af52f4a20b362b7656a
simonrobins <simonrobins@localhost>
2021-07-14 17:12:48 +0100

    Added promotion calculation functions.

--- src/com/simonrobins/pe/PromotionEngine.java
+++ src/com/simonrobins/pe/PromotionEngine.java
@@ -4,14 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PromotionEngine {
+	
+	private interface Function
+	{
+		public Double calculate(Map<Character, Integer> skuMap);
+	}
+	
+	private static Function[] orderCostFunctions = { 
+			new FunctionA(),
+			new FunctionB(),
+			new FunctionCD()
+	};
+	
 	public static Double calculateOrderCost(final String skus) {
 
 		Map<Character, Integer> groupedSKUs = countUniqueSKUs(skus);
 
 		Double cost = 0.0;
 
-		for (Map.Entry<Character, Integer> entry : groupedSKUs.entrySet()) {
-			
+		for(Function function : orderCostFunctions)
+		{
+			cost += function.calculate(groupedSKUs);
 		}
 
 		return cost;
@@ -19,6 +32,10 @@ public class PromotionEngine {
 
 	protected static Map<Character, Integer> countUniqueSKUs(final String skus) {
 		Map<Character, Integer> groupedSKUs = new HashMap<>();
+		groupedSKUs.put('A', 0);
+		groupedSKUs.put('B', 0);
+		groupedSKUs.put('C', 0);
+		groupedSKUs.put('D', 0);
 
 		for (int i = 0; i < skus.length(); i++) {
 			char sku = skus.charAt(i);
@@ -41,5 +58,50 @@ public class PromotionEngine {
 		}
 
 		return groupedSKUs;
+	}
+
+	private static class FunctionA implements Function
+	{
+		@Override
+		public Double calculate(Map<Character, Integer> skuMap) {
+			Integer numberOfAs = skuMap.get('A');
+
+			Integer numberOfPromotions = numberOfAs / 3;
+			Integer numberOfUnits = numberOfAs % 3;
+			
+			return numberOfPromotions * 130.0 + numberOfUnits * 50.0;
+		}		
+	}
+
+	private static class FunctionB implements Function
+	{
+		@Override
+		public Double calculate(Map<Character, Integer> skuMap) {
+			Integer numberOfBs = skuMap.get('B');
+
+			Integer numberOfPromotions = numberOfBs / 2;
+			Integer numberOfUnits = numberOfBs % 2;
+			
+			return numberOfPromotions * 45.0 + numberOfUnits * 30.0;
+		}		
+	}
+
+	private static class FunctionCD implements Function
+	{
+		@Override
+		public Double calculate(Map<Character, Integer> skuMap) {
+			Integer numberOfCs = skuMap.get('C');
+			Integer numberOfDs = skuMap.get('D');
+
+			if(numberOfCs > numberOfDs)
+			{
+				return  ((numberOfCs - numberOfDs) * 45.0) + numberOfCs * 20.0;
+			}
+			else
+			{
+				return  ((numberOfDs - numberOfCs) * 45.0) + numberOfDs * 15.0;
+				
+			}
+		}		
 	}
 }
